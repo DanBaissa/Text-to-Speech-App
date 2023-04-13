b18b509478a6b08c697df456c8e3bebbaf1abb39
DanBaissa <danbaissa@gmail.com>
2023-04-13 15:09:07 -0400

    Now it adds the functionality to convert all pdfs in a folder into mp3s

--- Gui_app.py
+++ Gui_app.py
@@ -4,6 +4,7 @@ from tkinter import filedialog, ttk
 import pdfplumber
 from gtts import gTTS
 import threading
+import glob
 
 def extract_text_from_pdf(pdf_path, progress_callback=None):
     with pdfplumber.open(pdf_path) as pdf:
@@ -33,6 +34,11 @@ def browse_pdf():
     if file_path:
         pdf_path_var.set(file_path)
 
+def browse_folder():
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        folder_path_var.set(folder_path)
+
 def convert_to_mp3():
     pdf_path = pdf_path_var.get()
     if not pdf_path:
@@ -54,10 +60,31 @@ def convert_to_mp3():
 
     threading.Thread(target=conversion_thread, daemon=True).start()
 
+def convert_folder_to_mp3s():
+    folder_path = folder_path_var.get()
+    if not folder_path:
+        return
+
+    result_var.set("Working...")
+
+    def conversion_thread():
+        try:
+            pdf_files = glob.glob(os.path.join(folder_path, '*.pdf'))
+            for pdf_path in pdf_files:
+                output_path = os.path.splitext(pdf_path)[0] + '.mp3'
+                text = extract_text_from_pdf(pdf_path)
+                text_to_speech(text, output_path)
+            result_var.set("Complete")
+        except Exception as e:
+            result_var.set("Conversion Failed")
+
+    threading.Thread(target=conversion_thread, daemon=True).start()
+
 app = tk.Tk()
 app.title("PDF to MP3 Converter")
 
 pdf_path_var = tk.StringVar()
+folder_path_var = tk.StringVar()
 result_var = tk.StringVar()
 
 frame = tk.Frame(app, padx=10, pady=10)
@@ -72,10 +99,23 @@ pdf_entry.grid(row=0, column=1)
 pdf_button = tk.Button(frame, text="Browse", command=browse_pdf)
 pdf_button.grid(row=0, column=2)
 
-convert_button = tk.Button(frame, text="Convert to MP3", command=convert_to_mp3)
-convert_button.grid(row=1, column=1, pady=10)
+folder_label = tk.Label(frame, text="Folder:")
+folder_label.grid(row=1, column=0, sticky="e")
+
+folder_entry = tk.Entry(frame, width=40, textvariable=folder_path_var)
+folder_entry.grid(row=1, column=1)
+
+folder_button = tk.Button(frame, text="Browse", command=browse_folder)
+folder_button.grid(row=1, column=2)
+
+convert_button = tk.Button(frame, text="Convert PDF to MP3", command=convert_to_mp3)
+convert_button.grid(row=2, column=1, pady=10)
+
+convert_folder_button = tk.Button(frame, text="Convert Folder to MP3s", command=convert_folder_to_mp3s)
+convert_folder_button.grid(row=3, column=1, pady=10)
 
 result_label = tk.Label(frame, textvariable=result_var)
-result_label.grid(row=2, column=0, columnspan=3)
+result_label.grid(row=4, column=0, columnspan=3)
 
 app.mainloop()
+
